450980fb1015c811bbd9a5e4ac7eb533df8c15d4
Paul Ganssle <paul@ganssle.io>
2016-12-25 11:52:31 -0800

    Fix HTML tag stripper so that truncation truncates tags properly.

--- audio_feeder/page_generator.py
+++ audio_feeder/page_generator.py
@@ -2,7 +2,9 @@
 Page generator
 """
 from html.parser import HTMLParser
+import bisect
 import glob
+import io
 import math
 import os
 import string
@@ -57,19 +59,68 @@ class _TagStripper(HTMLParser):
         super().__init__(convert_charrefs=False)
 
         self.all_data = []
+        self.pos_counts = []
+        self.data_pos = 0
 
     def handle_data(self, d):
         self.all_data.append(d)
+        pos_start = self.get_docpos()
+
+        self.pos_counts.append((self.data_pos, pos_start))
+        self.data_pos += len(d)
+
+    def count_lines(self, html_str):
+        """
+        It does not seem that there is any way to retrieve the positions of
+        tags when parsing HTML in any major library, so we'll need to use a
+        two-pass solution instead - first go through each line and get the
+        position in the string of the line.
+        """
+        html_io = io.StringIO(html_str)
+        line_positions = {}
+        for ii, line in enumerate(html_io):
+            line_positions[ii] = html_io.tell() - len(line)
+
+        self.line_positions = line_positions
+
+    def get_docpos(self):
+        line_no, offset = self.getpos()
+
+        # Insanely, the line number is a 1-based index.
+        line_no -= 1
+        return self.line_positions[line_no] + offset
+
+    @classmethod
+    def feed_stripper(cls, html_str):
+        ts = cls()
+        ts.count_lines(html_str)
+        ts.feed(html_str)
+        ts.close()
+
+        return ts
 
     def get_data(self):
         return ''.join(self.all_data)
 
-    @classmethod
-    def strip_tags(cls, html_str):
-        ts = cls()
-        ts.feed(html_str)
+    def get_unstripped_pos(self, pos):
+        """
+        After the feed has been processed.
 
-        return ts.get_data()
+        :param pos:
+            The position of the "stripped" tag
+
+        :return:
+            Returns the position in the "unstripped" string.
+        """
+        # Find out which segment we're in - always get the position to the
+        # right of the one we want to be in.
+        max_pos = self.pos_counts[-1][1]
+        pos_counts_pos = bisect.bisect_right(self.pos_counts, (pos, max_pos))
+        pos_counts_pos -= 1
+
+        stripped_base, unstripped_base = self.pos_counts[pos_counts_pos]
+
+        return unstripped_base + (pos - stripped_base)
 
 
 class QRGenerator:
@@ -212,7 +263,8 @@ class EntryRenderer:
 
     def truncation_point(self, description):
         # Strip out all HTML
-        raw_chars = _TagStripper.strip_tags(description)
+        stripper = _TagStripper.feed_stripper(description)
+        raw_chars = stripper.get_data()
 
         config = get_configuration()
         base_truncation_point = config['base_truncation_point']
@@ -227,7 +279,12 @@ class EntryRenderer:
 
             word_offset += 1
 
-        return base_truncation_point + word_offset
+        truncation_point = base_truncation_point + word_offset
+
+        # Now get the position in the "unstripped" string
+        orig_pos = stripper.get_unstripped_pos(truncation_point)
+
+        return orig_pos
 
 
 class NavItem:
@@ -284,5 +341,4 @@ class NavGenerator:
 
 class FailedResolutionError(IOError):
     pass
-
 

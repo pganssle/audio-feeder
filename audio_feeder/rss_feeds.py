a67c5aca7f89fcc27dab3c5e86f37867a3ba9f6d
Paul Ganssle <paul@ganssle.io>
2016-12-27 16:47:53 -0500

    Added function to generate RSS feed items, plus fixes to RSS feed templates.

--- audio_feeder/rss_feeds.py
+++ audio_feeder/rss_feeds.py
@@ -1,10 +1,15 @@
 """
 RSS Feed generators
 """
+from datetime import timedelta
 import hashlib
 import math
 import os
 import random
+
+from .config import read_from_config
+from . import directory_parser as dp
+from . import database_handler as dh
 
 def hash_random(fpath, hashseed, hash_amt=2**20, block_size=2**12,
                 hash_func=hashlib.sha256):
@@ -64,6 +69,54 @@ def hash_random(fpath, hashseed, hash_amt=2**20, block_size=2**12,
     return hash_obj.hexdigest()
 
 
+def load_feed_items(entry_obj, loader=dp.AudiobookLoader):
+    """
+    Creates feed items from a directory.
 
+    :param entry_obj:
+        A :class:`object_handlers.Entry` object.
+
+    :param loader:
+        A subclass of :class:`directory_parser.BaseAudioLoader`.
+
+    :return:
+        Returns a list of feed items for use with the RSS templates.
+
+        .. note::
+            The "url" parameter of each feed item will be relative to the static
+            media url - this should be modified on the fly when actually
+            generating the rss files.
+    """
+    base_path = read_from_config('static_media_path')
+
+    audio_dir = os.path.join(base_path, entry_obj.path)
+    if not os.path.exists(audio_dir):
+        raise ItemNotFoundError('Could not find item: {}'.format(audio_dir))
+
+    pub_date = entry_obj.date_added
+    data_obj = dh.get_database_table(entry_obj.table)[entry_obj.data_id]
+
+    audio_files = loader.audio_files(audio_dir)
+
+    feed_items = []
+    for ii, audio_file in enumerate(audio_files):
+        feed_item = {}
+
+        relpath = os.path.relpath(audio_file, base_path)
+
+        file_size =  os.path.getsize(audio_file)
+        feed_item['size'] = file_size
+        feed_item['url'] = relpath
+        feed_item['pubdate'] = pub_date + timedelta(minutes=ii)
+        feed_item['desc'] = data_obj.description or ''
+        feed_item['guid'] = hash_random(audio_file, entry_obj.hashseed)
+
+        feed_items.append(feed_item)
+
+    return feed_items
+
+
+class ItemNotFoundError(ValueError):
+    pass
 
 
--- templates/RSS/RSS_feed.tpl
+++ templates/RSS/RSS_feed.tpl
@@ -2,31 +2,30 @@
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
  
     <channel>
-
         <title>{{ channel_title }}</title>
         <description>{{ channel_desc }}</description>
         <link>{{ channel_link }}</link>
-        {{ cover_image_tag }}
+        {% if not cover_image is sameas none %}<image>{{ cover_image }}</image>{% endif %} 
         <language>en-us</language>
         <lastBuildDate>{{ build_date }}</lastBuildDate>
         <pubDate>{{ pub_date }}</pubDate>
         <docs>http://blogs.law.harvard.edu/tech/rss</docs>
-        
+
         <itunes:author>{{ author }}</itunes:author>
         <itunes:summary>{{ channel_desc }}</itunes:summary>
 
         <itunes:explicit>No</itunes:explicit>
 
-        {{ itunes_cover_image_tag }}
-        
+        {% if not cover_image is sameas none %}<itunes:image href="{{ cover_image }}" />{% endif %}
+
         {% for item in items %}
         <item>
-            <title>{item.title}</title>
-            <link>{channel_link}</link>
-            <description>{item.desc}</description>
-            <pubDate>{item.pubdate}</pubDate>
-            <guid>{item.guid}</guid>
-            <enclosure url="{item.url}" length="{item.size}" type="audio/mpeg"/>
+            <title>{{ item.title }}</title>
+            <link>{{ channel_link }}</link>
+            <description>{{ item.desc }}</description>
+            <pubDate>{{ item.pubdate }}</pubDate>
+            <guid>{{ item.guid }}</guid>
+            <enclosure url="{{ item.url }}" length="{{ item.size }}" type="audio/mpeg"/>
         </item>
         {% endfor %}
 

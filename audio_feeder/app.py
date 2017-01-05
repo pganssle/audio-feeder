092f2899c903714761dab47620783e3fff4f5716
Paul Ganssle <paul@ganssle.io>
2017-01-05 13:12:33 -0500

    Changed app logging.

--- audio_feeder/app.py
+++ audio_feeder/app.py
@@ -2,14 +2,33 @@
 from flask import Flask
 from .pages import root
 
-def create_app(load_db=True, populate_qr_cache=True, progressbar=True):
+import os
+import logging as log
+
+def create_app(load_db=True, populate_qr_cache=True, progressbar=False):
+    # Set up logging
+    log_level = os.environ.get('AF_LOGGING_LEVEL', None)
+    if log_level is not None:
+        log_levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
+        if log_level.upper() in log_levels:
+            log_level = getattr(log, log_level)
+
+            log.basicConfig(level=log_level)
+        else:
+            log.warning('Invalid log level: {}'.format(log_level.upper()))
+    else:
+        log.warning('No log level set, using default level.')
+
+    log.info('Creating Flask application')
     app = Flask(__name__)
     app.register_blueprint(root)
 
     # Now load the database if requested
     if load_db:
         from . import database_handler as dh
+        log.info('Loading database.')
         dh.get_database()       # This loads the database into memory.
+        log.info('Database loaded.')
 
     if populate_qr_cache:
         if progressbar:
@@ -18,6 +37,7 @@ def create_app(load_db=True, populate_qr_cache=True, progressbar=True):
                                         ' ', Timer(), ' ', ETA()])
             kwargs = {'pbar': pbar}
         else:
+            log.info('Populating QR cache.')
             kwargs = {}
 
         from .cache_utils import populate_qr_cache

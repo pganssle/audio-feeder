73d457289110a82a6028dc79e01dccc802a60086
Paul Ganssle <git@m.ganssle.io>
2022-11-01 14:08:29 -0400

    Add types to directory parser

--- src/audio_feeder/directory_parser.py
+++ src/audio_feeder/directory_parser.py
@@ -1,6 +1,10 @@
 import itertools
 import os
+import pathlib
 import re
+import typing
+
+from ._useful_types import PathType
 
 
 class BaseAudioLoader:
@@ -8,23 +12,28 @@ class BaseAudioLoader:
     A base class that defines the interface for all audio loader classes.
     """
 
-    def is_audio(cls, dirloc):
+    @classmethod
+    def is_audio(cls, dirloc: PathType) -> bool:
         raise NotImplementedError("Function must be defined in child classes")
 
-    def audio_files(cls, dir_loc):
+    @classmethod
+    def audio_files(cls, dir_loc: PathType) -> typing.Sequence[pathlib.Path]:
         raise NotImplementedError("Function must be defined in child classes.")
 
-    def audio_cover(cls, dir_loc):
+    @classmethod
+    def audio_cover(cls, dir_loc: PathType) -> typing.Optional[pathlib.Path]:
         raise NotImplementedError("Function must be defined in child classes.")
 
-    def parse_creator_names(cls, dir_loc):
+    @classmethod
+    def parse_creator_names(cls, creators: str) -> typing.Sequence[str]:
         raise NotImplementedError("Function must be defined in child classes")
 
-    def parse_audio_info(cls, dir_path):
+    @classmethod
+    def parse_audio_info(cls, dir_path: PathType) -> typing.Mapping[str, typing.Any]:
         raise NotImplementedError("Function must be implemented in child classes")
 
     @classmethod
-    def natural_sort_key(cls, value):
+    def natural_sort_key(cls, value: str) -> typing.Sequence[typing.Union[str, int]]:
         """
         This is a sort key to do a "natural" lexographic sort, the string is
         broken up into segments of strings and numbers, so that, e.g. `'Str 2'`
@@ -48,11 +57,26 @@ class AudiobookLoader(BaseAudioLoader):
     This is a more or less static class that can be used to load audiobooks.
     """
 
-    AUDIO_EXTENSIONS = [".mp3", ".mp4", ".ogg", ".ac3", ".aac", ".m4b", ".m4a"]
+    AUDIO_EXTENSIONS: typing.Final[typing.Sequence[str]] = (
+        ".mp3",
+        ".mp4",
+        ".ogg",
+        ".ac3",
+        ".aac",
+        ".m4b",
+        ".m4a",
+    )
 
-    COVER_EXTENSIONS = [".png", ".jpg", ".svg", ".gif", ".tif", ".bmp"]
+    COVER_EXTENSIONS: typing.Final[typing.Sequence[str]] = (
+        ".png",
+        ".jpg",
+        ".svg",
+        ".gif",
+        ".tif",
+        ".bmp",
+    )
 
-    COVER_PATTERNS = [
+    COVER_PATTERNS: typing.Final[typing.Sequence[re.Pattern]] = [
         re.compile("$.*\-Cover^", re.IGNORECASE),
         re.compile("cover", re.IGNORECASE),
     ]
@@ -64,7 +88,7 @@ class AudiobookLoader(BaseAudioLoader):
     )
 
     @classmethod
-    def is_audio(cls, dirloc):
+    def is_audio(cls, dirloc: PathType) -> bool:
         """
         Function to determine if a directory contains an audiobook. The
         assumption is that in this context, any directory containing at least
@@ -86,7 +110,7 @@ class AudiobookLoader(BaseAudioLoader):
         return False
 
     @classmethod
-    def audio_files(cls, dir_loc):
+    def audio_files(cls, dir_loc: PathType) -> typing.Sequence[pathlib.Path]:
         """
         Load all audiobook files and sort them appropriately.
 
@@ -103,10 +127,10 @@ class AudiobookLoader(BaseAudioLoader):
             if fname_ext in cls.AUDIO_EXTENSIONS:
                 o.append(os.path.join(dir_loc, fname))
 
-        return sorted(o, key=cls.natural_sort_key)
+        return list(map(pathlib.Path, sorted(o, key=cls.natural_sort_key)))
 
     @classmethod
-    def audio_cover(cls, dir_loc):
+    def audio_cover(cls, dir_loc: PathType) -> typing.Optional[pathlib.Path]:
         """
         Retrieve the best candidate for an audiobook cover.
 
@@ -139,10 +163,10 @@ class AudiobookLoader(BaseAudioLoader):
 
         mn, el, cover_loc = min(candidates)
 
-        return cover_loc
+        return pathlib.Path(cover_loc)
 
     @classmethod
-    def parse_creator_names(cls, authors):
+    def parse_creator_names(cls, creators: str) -> typing.Sequence[str]:
         """
         Splits author names by ',', '&' and 'and'
 
@@ -152,14 +176,14 @@ class AudiobookLoader(BaseAudioLoader):
         :return:
             Returns a :py:object:`list` of authors.
         """
-        o = authors.split(" & ")
+        o = creators.split(" & ")
         o = itertools.chain.from_iterable(x.split(" and ") for x in o)
         o = itertools.chain.from_iterable(x.split(", ") for x in o)
 
         return list(o)
 
     @classmethod
-    def parse_audio_info(cls, dir_path):
+    def parse_audio_info(cls, dir_path: PathType) -> typing.Mapping[str, typing.Any]:
         """
         Try to parse audiobook information the directory name.
         """
@@ -187,7 +211,12 @@ class AudiobookLoader(BaseAudioLoader):
         return audiobook_data
 
 
-def load_all_audio(base_dir, *, visited_dirs=None, audio_loader_class=AudiobookLoader):
+def load_all_audio(
+    base_dir: PathType,
+    *,
+    visited_dirs: typing.Optional[typing.Set[pathlib.Path]] = None,
+    audio_loader_class: typing.Type[BaseAudioLoader] = AudiobookLoader
+) -> typing.Sequence[pathlib.Path]:
     """
     Traverse a directory and find all the directories that contain an
     audiobook (as returned by is_audiobook)
@@ -205,13 +234,14 @@ def load_all_audio(base_dir, *, visited_dirs=None, audio_loader_class=AudiobookL
     """
     # We're not going to use os.walk here because we want to stop drilling
     # down once we've found an audiobook
+    base_dir = pathlib.Path(base_dir)
     audiobook_paths = []
     visited_dirs = visited_dirs or set()
-    visited_dirs.add(os.path.abspath(base_dir))
+    visited_dirs.add(pathlib.Path(os.path.abspath(base_dir)))
 
-    for subdir in os.listdir(base_dir):
-        dirpath = os.path.abspath(os.path.join(base_dir, subdir))
-        if dirpath in visited_dirs or not os.path.isdir(dirpath):
+    for subdir in base_dir.iterdir():
+        dirpath = (base_dir / subdir).absolute()
+        if dirpath in visited_dirs or not dirpath.is_dir():
             continue  # Avoid infinite recursion
 
         if audio_loader_class.is_audio(dirpath):

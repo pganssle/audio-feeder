eec2e27e8d0269e68045598c0632663ab9f187d5
Paul Ganssle <git@m.ganssle.io>
2022-11-06 13:33:06 -0500

    Expand tests to include YAML database handler

--- tests/test_database_handlers.py
+++ tests/test_database_handlers.py
@@ -5,24 +5,42 @@ import typing
 
 import pytest
 
+from audio_feeder import _db_types
 from audio_feeder import object_handler as oh
 from audio_feeder import sql_database_handler as sdh
+from audio_feeder import yaml_database_handler as ydh
 
 
 @pytest.fixture
-def db(tmp_path) -> typing.Iterator[pathlib.Path]:
+def sql_db(tmp_path) -> typing.Iterator[pathlib.Path]:
     db_loc = pathlib.Path(__file__).parent / "data/db.sqlite"
     copy_loc = tmp_path / "db.sqlite"
-
-    shutil.copyfile(db_loc, copy_loc)
 
     yield copy_loc
 
 
-def test_load_db(db: pathlib.Path) -> None:
-    handler = sdh.SqlDatabaseHandler(db)
+@pytest.fixture(
+    params=[
+        (pathlib.Path(__file__).parent / "data/db.sqlite", sdh.SqlDatabaseHandler),
+        (pathlib.Path(__file__).parent / "data/yaml_db", ydh.YamlDatabaseHandler),
+    ]
+)
+def db_handler(
+    request, tmp_path: pathlib.Path
+) -> typing.Iterator[_db_types.DatabaseHandler]:
+    db_loc, handler_type = request.param
+    copy_loc = tmp_path / db_loc.name
 
-    db_tables = handler.load_database()
+    if db_loc.is_dir():
+        shutil.copytree(db_loc, copy_loc)
+    else:
+        shutil.copy(db_loc, copy_loc)
+
+    yield handler_type(copy_loc)
+
+
+def test_load_db(db_handler: _db_types.DatabaseHandler) -> None:
+    db_tables = db_handler.load_database()
 
     assert len(db_tables["books"]) == 5
     for book in db_tables["books"].values():
@@ -50,9 +68,16 @@ def test_load_db(db: pathlib.Path) -> None:
     assert os.fspath(twof_entry.cover_images[0]).startswith("images/entry_cover_cache")
 
 
-def test_load_empty_database(tmp_path):
-    db = tmp_path / "db.sqlite"
+def test_load_empty_sql_database(tmp_path: pathlib.Path) -> None:
+    """Test that the SQL database handler can load an empty database.
 
+    This is the one point of difference between YAML and SQL database handlers;
+    the YAML database throws an error but SQL initializes the database for us.
+    The reason it is different is that the capability is not built in to the
+    YAML handler, and the YAML database is going to be deprecated, so this is
+    a YAGNI situation.
+    """
+    db = tmp_path / "db.sqlite"
     handler = sdh.SqlDatabaseHandler(db)
 
     db_tables = handler.load_database()
@@ -60,19 +85,32 @@ def test_load_empty_database(tmp_path):
         assert len(table) == 0
 
 
-def test_load_table(db: pathlib.Path) -> None:
-    handler = sdh.SqlDatabaseHandler(db)
+def test_load_empty_yaml_database(tmp_path: pathlib.Path) -> None:
+    """Test that the YAML database fails when loading a missing database.
 
-    books = handler.load_table("books")
+    This is the one point of difference between YAML and SQL database handlers;
+    the YAML database throws an error but SQL initializes the database for us.
+    The reason it is different is that the capability is not built in to the
+    YAML handler, and the YAML database is going to be deprecated, so this is
+    a YAGNI situation.
+    """
+    db = tmp_path / "db/"
+    handler = ydh.YamlDatabaseHandler(db)
+
+    with pytest.raises(ValueError):
+        handler.load_database()
+
+
+def test_load_table(db_handler: _db_types.DatabaseHandler) -> None:
+    books = db_handler.load_table("books")
 
     assert len(books) == 5
     for book in books.values():
         assert isinstance(book, oh.Book)
 
 
-def test_save_database(db: pathlib.Path) -> None:
-    handler = sdh.SqlDatabaseHandler(db)
-    db_tables = handler.load_database()
+def test_save_database(db_handler: _db_types.DatabaseHandler) -> None:
+    db_tables = db_handler.load_database()
 
     db_tables["books"][21144] = oh.Book(
         id=21144,
@@ -81,18 +119,17 @@ def test_save_database(db: pathlib.Path) -> None:
         author_ids=[496925],
     )
 
-    handler.save_database(db_tables)
+    db_handler.save_database(db_tables)
 
-    db_tables_reload = handler.load_database()
+    db_tables_reload = db_handler.load_database()
 
     assert db_tables is not db_tables_reload
 
     assert 21144 in db_tables["books"]
 
 
-def test_save_table(db: pathlib.Path) -> None:
-    handler = sdh.SqlDatabaseHandler(db)
-    db_tables = handler.load_database()
+def test_save_table(db_handler: _db_types.DatabaseHandler) -> None:
+    db_tables = db_handler.load_database()
 
     db_tables["authors"][2444] = oh.Author(
         id=2444,
@@ -107,24 +144,23 @@ def test_save_table(db: pathlib.Path) -> None:
         authors=["H.G. Wells"],
         author_ids=[2444],
     )
-    handler.save_table("authors", db_tables["authors"])
+    db_handler.save_table("authors", db_tables["authors"])
 
-    db_tables_reload = handler.load_database()
+    db_tables_reload = db_handler.load_database()
     assert 2444 in db_tables_reload["authors"]
     assert 11243 not in db_tables_reload["books"]
 
 
-def test_save_database_remove(db: pathlib.Path) -> None:
-    handler = sdh.SqlDatabaseHandler(db)
-    db_tables = handler.load_database()
+def test_save_database_remove(db_handler: _db_types.DatabaseHandler) -> None:
+    db_tables = db_handler.load_database()
 
     book_ids = [book_id for book_id in db_tables["books"].keys()]
     for book_id in book_ids[:3]:
         del db_tables["books"][book_id]
 
-    handler.save_table("books", db_tables["books"])
+    db_handler.save_table("books", db_tables["books"])
 
-    books_table = handler.load_table("books")
+    books_table = db_handler.load_table("books")
 
     assert books_table is not db_tables["books"]
 
